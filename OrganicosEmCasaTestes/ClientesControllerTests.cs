57f474462f817108dd3da2d5efa1984b9cadb759
Daniela Milagres <daniela.milagres@localiza.com>
2019-03-16 19:31:14 -0300

    Configurações testes e primeiros métodos.

--- OrganicosEmCasaTestes/ClientesControllerTests.cs
+++ OrganicosEmCasaTestes/ClientesControllerTests.cs
@@ -1,18 +1,50 @@
 ﻿using System;
+using System.IO;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OrganicosEmCasa.Controllers;
+using OrganicosEmCasa.Models;
 
 namespace OrganicosEmCasaTestes
 {
     [TestClass]
     public class ClientesControllerTests
     {
+        public TestContext TestContext { get; set; }
+        private readonly ClientesController Controller;
+
+        [ClassInitialize]
+        public static void SetUp(TestContext context)
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(context.TestDeploymentDir, string.Empty));
+        }
+
         [TestMethod]
         public void TestIndexView()
         {
             var controller = new ClientesController();
             var resultado = controller.Index() as ViewResult;
+            Assert.IsNotNull(resultado);
+        }
+
+        [TestMethod]
+        public void TestCreateAction()
+        {
+            var cliente = new Cliente
+            {
+                Nome = "Daniela Milagres",
+                Endereco = "Avenida do Contorno 3000",
+                UF = OrganicosEmCasa.Utils.Estados.MG,
+                Cidade = "Belo Horizonte",
+                CPF = "486.475.170-67",
+                CEP = "30.000-000",
+                Telefone = "(31)98877-9878"
+            };
+
+            var controller = new ClientesController();
+            var resultado = controller.Create(cliente);
+
+            Assert.IsNotNull(resultado);
         }
     }
 }
