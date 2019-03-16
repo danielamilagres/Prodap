ec5885e59d1958e4474a54011b66f414ae9b7ca1
Daniela Milagres <daniela.milagres@localiza.com>
2019-03-16 15:48:32 -0300

    Projeto de testes

---
+++ OrganicosEmCasaTestes/ClientesControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrganicosEmCasa.Controllers;
+
+namespace OrganicosEmCasaTestes
+{
+    [TestClass]
+    public class ClientesControllerTests
+    {
+        [TestMethod]
+        public void TestIndexView()
+        {
+            var controller = new ClientesController();
+            var resultado = controller.Index() as ViewResult;
+        }
+    }
+}
---
+++ OrganicosEmCasaTestes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrganicosEmCasaTestes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OrganicosEmCasaTestes")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25f5a27e-dc50-49ab-a52a-629395eeb49c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
