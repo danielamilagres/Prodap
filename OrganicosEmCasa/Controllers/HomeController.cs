96dc6a81b12ceb60e7c0095758524bb2eaabb803
Daniela Milagres <daniela.milagres@localiza.com>
2019-03-16 12:30:36 -0300

    View/Home - Alterações de Layout e apresentação

--- OrganicosEmCasa/Controllers/HomeController.cs
+++ OrganicosEmCasa/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrganicosEmCasa.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,10 @@ namespace OrganicosEmCasa.Controllers
 {
     public class HomeController : Controller
     {
+        private OrganicosEmCasaDBContext db = new OrganicosEmCasaDBContext();
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(db.Categorias.ToList());
         }
     }
 }
